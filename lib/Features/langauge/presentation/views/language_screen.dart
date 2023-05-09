8b28acd1dad4f2c6db0091b6fa8f0ee7545ad241
AsmaaMohamed81 <eng.asmaa.mohammed13593@gmail.com>
2023-05-09 10:31:03 +0200

    text field as a test

--- lib/Features/langauge/presentation/views/language_screen.dart
+++ lib/Features/langauge/presentation/views/language_screen.dart
@@ -2,6 +2,7 @@ import 'package:fingerPrint/Features/Splash/presentation/manger/locale_cubit/loc
 import 'package:fingerPrint/core/locale/app_localizations.dart';
 import 'package:fingerPrint/core/utils/commons.dart';
 import 'package:fingerPrint/core/utils/gaps.dart';
+import 'package:fingerPrint/core/utils/text_Form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -16,6 +17,7 @@ class LanguageScreen extends StatefulWidget {
 
 class _LanguageScreenState extends State<LanguageScreen> {
   late AppLocalizations locale;
+  final _phoneNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,25 +25,59 @@ class _LanguageScreenState extends State<LanguageScreen> {
 
     return Scaffold(
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-                onTap: () {
-                  Commons.showToast(context,
-                      message: locale.translate('english')!);
+            Container(
+              decoration: BoxDecoration(
+                  border:
+                      Border.all(color: Colors.grey.withOpacity(0.2), width: 1),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16)),
+              padding: const EdgeInsets.all(6),
+              child: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "S.of(context).please_enter_your_name";
+                  }
+                  return null;
+                },
+                controller: _phoneNumberController,
+                decoration: const InputDecoration(
+                  label: Text("daad"),
+                  focusedBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  focusedErrorBorder: InputBorder.none,
+                  // prefixIcon: Container(
+                  //     padding: const EdgeInsets.symmetric(vertical: 14),
+                  //     child: Icon(Icons.abc)),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Commons.showToast(context,
+                          message: locale.translate('english')!);
 
-                  BlocProvider.of<LocaleCubit>(context).toEnglish();
-                },
-                child: Text(locale.translate('english')!)),
-            Gaps.hGap30,
-            GestureDetector(
-                onTap: () {
-                  Commons.showToast(context,
-                      message: locale.translate('arabic')!);
-                  BlocProvider.of<LocaleCubit>(context).toArabic();
-                },
-                child: Text(locale.translate('arabic')!))
+                      BlocProvider.of<LocaleCubit>(context).toEnglish();
+                    },
+                    child: Text(locale.translate('english')!)),
+                Gaps.hGap30,
+                GestureDetector(
+                    onTap: () {
+                      Commons.showToast(context,
+                          message: locale.translate('arabic')!);
+                      BlocProvider.of<LocaleCubit>(context).toArabic();
+                    },
+                    child: Text(locale.translate('arabic')!))
+              ],
+            ),
           ],
         ),
       ),
