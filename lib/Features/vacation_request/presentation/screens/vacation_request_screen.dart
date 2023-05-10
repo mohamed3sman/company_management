d11979b6c035a40b2b4cfa98ba29fab5ff486ec3
engahmed309 <a.abdelwahed@gmail.com>
2023-05-10 14:13:15 +0300

    custom date picker not finished

--- lib/Features/vacation_request/presentation/screens/vacation_request_screen.dart
+++ lib/Features/vacation_request/presentation/screens/vacation_request_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../core/utils/gaps.dart';
 import '../../../../core/widgets/custom_orders_raw_icon.dart';
+import '../widgets/custom_date_picker.dart';
 
 class VacationRequestScreen extends StatelessWidget {
   const VacationRequestScreen({super.key});
@@ -11,66 +12,73 @@ class VacationRequestScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        
         height: MediaQuery.of(context).size.height * .87,
         child: SingleChildScrollView(
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //ASK ENG ASMAA WHICH IS BETTER ?
-                // IconButton(
-                //   onPressed: () {},
-                //   icon: Icon(
-                //     Icons.notifications_none_rounded,
-                //     size: 30,
-                //   ),
-                // ),
-                Expanded(
-                  child: Directionality(
-                    textDirection: TextDirection.rtl,
-                    //   color: Colors.red,
-                    child: ListTile(
-                      onTap: () {
-                        print("List tile");
-                      },
-                      // selectedColor: Colors.red,
-                      title: Text(
-                        "أهلا وسهلا",
-                        style:
-                            TextStyle(color: Color(0xff9d9d9e), fontSize: 13),
-                      ),
-                      leading: Icon(Icons.person_pin),
-                      trailing: IconButton(
-                        onPressed: () {
-                          print("new notification");
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //ASK ENG ASMAA WHICH IS BETTER ?
+                  // IconButton(
+                  //   onPressed: () {},
+                  //   icon: Icon(
+                  //     Icons.notifications_none_rounded,
+                  //     size: 30,
+                  //   ),
+                  // ),
+                  Expanded(
+                    child: Directionality(
+                      textDirection: TextDirection.rtl,
+                      //   color: Colors.red,
+                      child: ListTile(
+                        onTap: () {
+                          print("List tile");
                         },
-                        icon: Icon(
-                          Icons.notifications_none_rounded,
-                          size: 30,
-                          color: Color(0xff707070),
+                        // selectedColor: Colors.red,
+                        title: Text(
+                          "أهلا وسهلا",
+                          style:
+                              TextStyle(color: Color(0xff9d9d9e), fontSize: 13),
                         ),
-                      ),
-                      subtitle: Text(
-                        "أحمد محمد عبدالرحمن",
-                        style:
-                            TextStyle(color: Color(0xff1d1d1d), fontSize: 15),
+                        leading: Icon(Icons.person_pin),
+                        trailing: IconButton(
+                          onPressed: () {
+                            print("new notification");
+                          },
+                          icon: Icon(
+                            Icons.notifications_none_rounded,
+                            size: 30,
+                            color: Color(0xff707070),
+                          ),
+                        ),
+                        subtitle: Text(
+                          "أحمد محمد عبدالرحمن",
+                          style:
+                              TextStyle(color: Color(0xff1d1d1d), fontSize: 15),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Gaps.vGap8,
-            Text("طلب إجازة",
+                ],
+              ),
+              Gaps.vGap8,
+              Text(
+                "طلب إجازة",
                 style: TextStyle(
                   color: Color(0xff000000),
                   fontSize: 25,
                   fontWeight: FontWeight.w400,
-                )),
-                CustomOrdersRawIcon(rawText: "نوع الإجازة",rawIcon: Icon(Icons.abc)),
-                CustomDropDownList()
-          ]),
+                ),
+              ),
+              CustomOrdersRawIcon(
+                  rawText: "نوع الإجازة", rawIcon: Icon(Icons.holiday_village)),
+              CustomDropDownList(),
+              CustomOrdersRawIcon(
+                  rawText: "تاريخ بداية الإجازة", rawIcon: Icon(Icons.holiday_village))
+          , CustomDatePicker(),
+            ],
+          ),
         ),
       ),
     );
