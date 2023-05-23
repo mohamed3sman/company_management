687a91fbdabb13a3c416715fb9a591ba5b045a4a
engahmed309 <a.abdelwahed@gmail.com>
2023-05-23 12:59:43 +0300

    editing bottom sgeet

--- lib/Features/order_details/procedures/presentaion/widgets/custom_row.dart
+++ lib/Features/order_details/procedures/presentaion/widgets/custom_row.dart
@@ -5,8 +5,10 @@ class CustomOrderItem extends StatelessWidget {
   final String titleText;
   final String subTitleText;
   final String imgscr;
+  final bool hasStatusIcon;
   const CustomOrderItem(
       {Key? key,
+      required this.hasStatusIcon,
       required this.titleText,
       required this.subTitleText,
       required this.imgscr})
@@ -14,21 +16,47 @@ class CustomOrderItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return ListTile(
       leading: Container(
         height: 45,
         width: 45,
-        decoration:  BoxDecoration(
+        decoration: BoxDecoration(
           color: kPrimaryColor.withOpacity(.1),
-          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
         ),
         child: Center(
-          child: Image.asset(imgscr,color: kPrimaryColor,),
+          child: Image.asset(
+            imgscr,
+            color: kPrimaryColor,
+          ),
         ),
       ),
-      title: Text(titleText,style: TextStyle(fontSize: 15,),),
-      subtitle: Text(subTitleText,style: TextStyle(fontSize: 13,)),
+      title: Text(
+        titleText,
+        style: const TextStyle(
+          fontSize: 15,
+        ),
+      ),
+      subtitle: Row(
+        children: [
+          hasStatusIcon
+              ? const Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3),
+                  child: Icon(
+                    Icons.circle,
+                    color: Color(0xffF1DA0B),
+                    size: 13,
+                  ),
+                )
+              : Container(),
+          Text(
+            subTitleText,
+            style: const TextStyle(
+              fontSize: 13,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Features/order_details/procedures/presentaion/widgets/showing_bottom_sheet.dart
+++ lib/Features/order_details/procedures/presentaion/widgets/showing_bottom_sheet.dart
@@ -1,6 +1,10 @@
+import 'package:fingerPrint/core/utils/gaps.dart';
 import 'package:fingerPrint/core/widgets/custom_button.dart';
-import 'package:fingerPrint/core/widgets/custom_login_text_field.dart';
+import 'package:fingerPrint/core/widgets/custom_orders_raw_icon.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/constants.dart';
+import '../../../../../core/widgets/custom_request_text_field.dart';
 
 class ShowModalBottomSheet extends StatefulWidget {
   const ShowModalBottomSheet({super.key});
@@ -14,22 +18,37 @@ String checkoption = "قبول";
 class _ShowModalBottomSheetState extends State<ShowModalBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              Text("الاجراءات"),
-              Icon(Icons.cancel),
-            ],
-          ),
-          Container(
-            alignment: Alignment.centerRight,
-            child: Column(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Gaps.vGap16,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+               const Text(
+                  "الاجراءات",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child:const Icon(Icons.cancel)),
+              ],
+            ),
+            Column(
               children: [
                 RadioListTile(
+                  activeColor: kPrimaryColor,
                   title: const Text("قبول"),
                   value: "قبول",
                   groupValue: checkoption,
@@ -40,6 +59,7 @@ class _ShowModalBottomSheetState extends State<ShowModalBottomSheet> {
                   },
                 ),
                 RadioListTile(
+                  activeColor: kPrimaryColor,
                   title: const Text("رفض"),
                   value: "رفض",
                   groupValue: checkoption,
@@ -51,18 +71,33 @@ class _ShowModalBottomSheetState extends State<ShowModalBottomSheet> {
                 ),
               ],
             ),
-          ),
-          CustomLoginTextField(
-            stringInTextField: "ملاحظات",
-            textInputType: TextInputType.text,
-            obscureText: false,
-            height: MediaQuery.of(context).size.height * 0.15,
-          ),
-          CustomButton(
-              screenWidth: MediaQuery.of(context).size.width * 0.7,
-              buttonTapHandler: () {},
-              buttonText: "حفظ")
-        ],
+            Divider(
+              thickness: 2,
+            ),
+            CustomOrdersRawIcon(
+              rawText: "ملاحظات",
+              iconImagePath: "assets/icons/notes_icon.png",
+            ),
+            Gaps.vGap5,
+            CustomRequestsTextField(
+              containerHeight: MediaQuery.of(context).size.height * .08,
+              hintTextField: "",
+            ),
+
+            // CustomLoginTextField(
+            //   width: double.infinity,
+            //   stringInTextField: "ملاحظات",
+            //   textInputType: TextInputType.text,
+            //   obscureText: false,
+            //   height: MediaQuery.of(context).size.height * 0.15,
+            // ),
+            Gaps.vGap10,
+            CustomButton(
+                screenWidth: double.infinity,
+                buttonTapHandler: () {},
+                buttonText: "حفظ")
+          ],
+        ),
       ),
     );
   }
